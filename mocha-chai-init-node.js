bef84017bb7029a4f6dd9eb79d774105e6648a33
Offirmo <offirmo.net@gmail.com>
2017-09-26 10:22:00 +1000

    code style

--- mocha-chai-init-node.js
+++ mocha-chai-init-node.js
@@ -4,7 +4,7 @@ var Mocha = require('mocha')
 var sinon = require('sinon')
 var chai = require('chai')
 var sinon_chai = require('sinon-chai')
-var chai_as_promised = require("chai-as-promised")
+var chai_as_promised = require('chai-as-promised')
 var chai_subset = require('chai-subset')
 var chai_moment = require('chai-moment')
 
@@ -13,9 +13,11 @@ var chai_moment = require('chai-moment')
 global.expect = chai.expect
 global.sinon = sinon
 
+// activate chai extensions
 chai.use(sinon_chai)
 chai.use(chai_as_promised)
 chai.use(chai_subset)
 chai.use(chai_moment)
 
+// convenience
 console.log('* Starting tests...')
